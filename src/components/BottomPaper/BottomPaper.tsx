478c750d16483695ddb8057a8d2d4a5db88ea79c
spbills <spencebills0@gmail.com>
2022-11-23 14:56:10 -0500

    Fixed box shadow prop

--- src/components/BottomPaper/BottomPaper.tsx
+++ src/components/BottomPaper/BottomPaper.tsx
@@ -28,7 +28,7 @@ const BottomPaper = ({ show = true, onClose, boxShadow, backgroundColor, childre
     <animated.div {...handlers} style={{
       translateY,
       backgroundColor: backgroundColor ?? "white",
-      boxShadow: "0px 3px 3px -2px rgb(0 0 0 / 20%), 0px 3px 4px 0px rgb(0 0 0 / 14%), 0px 1px 8px 0px rgb(0 0 0 / 12%)",
+      boxShadow: boxShadow ?? "0px 3px 3px -2px rgb(0 0 0 / 20%), 0px 3px 4px 0px rgb(0 0 0 / 14%), 0px 1px 8px 0px rgb(0 0 0 / 12%)",
       position: "absolute",
       bottom: "0",
       left: "0",
